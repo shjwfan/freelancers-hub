9005ecbff58fef5e496049b14515509825593ec3
shjwfan <shjwfan@proton.me>
2023-11-04 17:33:17 +0600

    get access token and refresh token with axios and react-hook-form

--- web/src/main/resources/webapp/src/LoginForm.tsx
+++ web/src/main/resources/webapp/src/LoginForm.tsx
@@ -1,6 +1,38 @@
+import { SubmitHandler, useForm } from 'react-hook-form';
+import axios from 'axios';
+
+type Credentials = {
+  username: string;
+  password: string;
+};
+
+type Token = {
+  accessToken: string;
+  refreshToken: string;
+};
+
+const PATTERN = /^[a-zA-Z0-9!@#$%^&*()_+{}[\]:;<>,.?~|\\/-]{4,32}$/g;
+
 const LoginForm = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Credentials>();
+  const onSubmit: SubmitHandler<Credentials> = (credentials: Credentials) => {
+    const url = new URL('/api/v1/login', window.origin);
+    url.searchParams.set('username', credentials.username);
+    url.searchParams.set('password', credentials.password);
+
+    axios
+      .get<Token>(url.toString())
+      .then(response => response.data)
+      .then(data => console.log(data))
+      .catch(error => console.error(error));
+  };
+
   return (
-    <>
+    <form onSubmit={e => void handleSubmit(onSubmit)(e)}>
       <div className='row'>
         <div className='col-3 m-auto mt-5 text-center'>
           <h2>Login</h2>
@@ -22,10 +54,21 @@ const LoginForm = () => {
               Username:
             </label>
             <input
-              className='form-control'
+              className={`form-control ${errors.username ? 'is-invalid' : ''}`}
               id='username-input'
               placeholder='Enter username'
+              {...register('username', {
+                required: true,
+                pattern: PATTERN,
+              })}
             />
+            {errors.username && (
+              <div className={'invalid-feedback'}>
+                Username is required, must consist of only English alphabet
+                and/or numbers and/or symbols and be between 4 and 32
+                characters.
+              </div>
+            )}
           </div>
           <div className='form-group mt-5'>
             <label
@@ -35,18 +78,29 @@ const LoginForm = () => {
               Password:
             </label>
             <input
-              className='form-control'
+              className={`form-control ${errors.password ? 'is-invalid' : ''}`}
               id='password-input'
               placeholder='Enter password'
               type='password'
+              {...register('password', {
+                required: true,
+                pattern: PATTERN,
+              })}
             />
+            {errors.password && (
+              <div className={'invalid-feedback'}>
+                Username is required, must consist of only English alphabet
+                and/or numbers and/or symbols and be between 4 and 32
+                characters.
+              </div>
+            )}
           </div>
           <button className='btn btn-primary mt-3' type='submit'>
             Login
           </button>
         </div>
       </div>
-    </>
+    </form>
   );
 };
 
