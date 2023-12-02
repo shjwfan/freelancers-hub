868c775d698e4fcebed00d4a1570007cb4686c1f
andrey <shjwfan@proton.me>
2023-12-02 21:31:13 +0600

    send started at email

---
+++ mail/src/main/java/org/shjwfan/mail/MailService.java
@@ -0,0 +1,75 @@
+package org.shjwfan.mail;
+
+import jakarta.mail.Authenticator;
+import jakarta.mail.Message;
+import jakarta.mail.Message.RecipientType;
+import jakarta.mail.PasswordAuthentication;
+import jakarta.mail.Session;
+import jakarta.mail.Transport;
+import jakarta.mail.internet.InternetAddress;
+import jakarta.mail.internet.MimeMessage;
+import java.util.Properties;
+import java.util.concurrent.CompletableFuture;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MailService {
+
+  private static final boolean MAIL_SMTP_AUTH = true;
+  private static final boolean MAIL_SMTP_STARTTLS_ENABLE = true;
+
+  private final Properties properties;
+  private final String username;
+  private final String password;
+  private final Logger logger;
+
+  public MailService(SmtpConfigurationProperties smtp) {
+    properties = new Properties();
+    properties.put("mail.smtp.auth", Boolean.toString(MAIL_SMTP_AUTH));
+    properties.put("mail.smtp.starttls.enable", Boolean.toString(MAIL_SMTP_STARTTLS_ENABLE));
+    properties.put("mail.smtp.host", smtp.getHost());
+    properties.put("mail.smtp.port", smtp.getPort());
+    username = smtp.getUsername();
+    password = smtp.getPassword();
+    logger = LoggerFactory.getLogger(getClass());
+  }
+
+  public void send(String recipient, String subject, String content) {
+    Session session = getSessionInstance();
+
+    String from = username;
+
+    CompletableFuture.runAsync(() -> {
+      try {
+        Message message = new MimeMessage(session);
+
+        message.setFrom(new InternetAddress(from));
+        message.setRecipient(RecipientType.TO, new InternetAddress(recipient));
+
+        message.setSubject(subject.trim());
+        message.setContent(content.trim(), "text/html");
+
+        Transport.send(message);
+
+        logger.trace("send email to {}, subject {}, content {}", recipient, subject, content);
+      } catch (Exception e) {
+        throw new IllegalStateException(e);
+      }
+    }).exceptionally(e -> {
+      logger.trace("can't send email to {}", recipient, e);
+      return null;
+    });
+  }
+
+  private Session getSessionInstance() {
+    return Session.getInstance(properties, new Authenticator() {
+
+      @Override
+      protected PasswordAuthentication getPasswordAuthentication() {
+        return new PasswordAuthentication(username, password);
+      }
+    });
+  }
+}
---
+++ mail/src/main/java/org/shjwfan/mail/SmtpConfigurationProperties.java
@@ -0,0 +1,46 @@
+package org.shjwfan.mail;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@ConfigurationProperties("freelancers-hub.smtp")
+@Configuration
+public class SmtpConfigurationProperties {
+
+  private String host = "127.0.0.1";
+  private String port = "587";
+  private String username = "";
+  private String password = "";
+
+  public String getHost() {
+    return host;
+  }
+
+  public void setHost(String host) {
+    this.host = host;
+  }
+
+  public String getPort() {
+    return port;
+  }
+
+  public void setPort(String port) {
+    this.port = port;
+  }
+
+  public String getUsername() {
+    return username;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+}
